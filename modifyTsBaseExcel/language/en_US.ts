5a6edaa23387bc721356c5bd16a2d1044ab9fde9
pzhao004 <lanquanyouyu@163.com>
2019-01-11 10:09:48 +0800

    Add files via upload

---
+++ modifyTsBaseExcel/language/en_US.ts
@@ -0,0 +1,90 @@
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE TS>
+<TS language="en_AS" version="2.1">
+    <context>
+        <name>BufferList2</name>
+        <message>
+            <location filename="../BufferList2.qml" line="32"/>
+            <source>id_text_PH</source>
+            <translation>pH</translation>
+        </message>
+        <message>
+            <location filename="../BufferList2.qml" line="60"/>
+            <location filename="../BufferList2.qml" line="87"/>
+            <location filename="../BufferList2.qml" line="114"/>
+            <source>Category</source>
+            <translation>Category</translation>
+        </message>
+    </context>
+    <context>
+        <name>BufferListScreen</name>
+        <message>
+            <location filename="../BufferListScreen.qml" line="98"/>
+            <source>Lable1</source>
+            <translation>label1</translation>
+        </message>
+        <message>
+            <location filename="../BufferListScreen.qml" line="111"/>
+            <source>Label2</source>
+            <translation>label2</translation>
+        </message>
+        <message>
+            <location filename="../BufferListScreen.qml" line="124"/>
+            <source>Label3</source>
+            <translation>label3</translation>
+        </message>
+        <message>
+            <location filename="../BufferListScreen.qml" line="137"/>
+            <source>Label4</source>
+            <translation>label4</translation>
+        </message>
+        <message>
+            <location filename="../BufferListScreen.qml" line="151"/>
+            <source>Go Back</source>
+            <translation>Go Back</translation>
+        </message>
+    </context>
+    <context>
+        <name>main</name>
+        <message>
+            <location filename="../main.qml" line="234"/>
+            <source>Start</source>
+            <translation>Start</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="249"/>
+            <source>Stop</source>
+            <translation>Stop</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="263"/>
+            <source>Store Mode</source>
+            <translation>Store Mode</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="277"/>
+            <source>Label4</source>
+            <translation>label4</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="290"/>
+            <source>Label5</source>
+            <translation>label5</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="304"/>
+            <source>Label6</source>
+            <translation>label6</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="394"/>
+            <source>English</source>
+            <translation>English</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="395"/>
+            <source>Chinese</source>
+            <translation>Chinese</translation>
+        </message>
+    </context>
+</TS>
---
+++ modifyTsBaseExcel/language/zh_CN.ts
@@ -0,0 +1,90 @@
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE TS>
+<TS language="zh_CN" version="2.1">
+    <context>
+        <name>BufferList2</name>
+        <message>
+            <location filename="../BufferList2.qml" line="32"/>
+            <source>pH</source>
+            <translation>pH</translation>
+        </message>
+        <message>
+            <location filename="../BufferList2.qml" line="60"/>
+            <location filename="../BufferList2.qml" line="87"/>
+            <location filename="../BufferList2.qml" line="114"/>
+            <source>Category</source>
+            <translation>类别</translation>
+        </message>
+    </context>
+    <context>
+        <name>BufferListScreen</name>
+        <message>
+            <location filename="../BufferListScreen.qml" line="98"/>
+            <source>Lable1</source>
+            <translation>标签1</translation>
+        </message>
+        <message>
+            <location filename="../BufferListScreen.qml" line="111"/>
+            <source>Label2</source>
+            <translation>标签2</translation>
+        </message>
+        <message>
+            <location filename="../BufferListScreen.qml" line="124"/>
+            <source>Label3</source>
+            <translation>标签3</translation>
+        </message>
+        <message>
+            <location filename="../BufferListScreen.qml" line="137"/>
+            <source>Label4</source>
+            <translation>标签4</translation>
+        </message>
+        <message>
+            <location filename="../BufferListScreen.qml" line="151"/>
+            <source>Go Back</source>
+            <translation>返回</translation>
+        </message>
+    </context>
+    <context>
+        <name>main</name>
+        <message>
+            <location filename="../main.qml" line="234"/>
+            <source>Start</source>
+            <translation>开始</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="249"/>
+            <source>Stop</source>
+            <translation>停止</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="263"/>
+            <source>Store Mode</source>
+            <translation>存储方式</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="277"/>
+            <source>Label4</source>
+            <translation>标签4</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="290"/>
+            <source>Label5</source>
+            <translation>标签5</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="304"/>
+            <source>Label6</source>
+            <translation>标签6</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="394"/>
+            <source>English</source>
+            <translation>英语</translation>
+        </message>
+        <message>
+            <location filename="../main.qml" line="395"/>
+            <source>Chinese</source>
+            <translation>中文</translation>
+        </message>
+    </context>
+</TS>
