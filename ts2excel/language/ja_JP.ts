ecb61641d309f9824c437dd7c684bb12593c78b6
JIN-DAN-QI <lanquanyouyu@163.com>
2019-01-11 10:55:53 +0800

    Add files via upload

---
+++ ts2excel/language/ja_JP.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>BufferList2</name>
+    <message>
+        <location filename="../BufferList2.qml" line="32"/>
+        <source>pH</source>
+        <translation>pH</translation>
+    </message>
+    <message>
+        <location filename="../BufferList2.qml" line="60"/>
+        <location filename="../BufferList2.qml" line="87"/>
+        <location filename="../BufferList2.qml" line="114"/>
+        <source>Category</source>
+        <translation>カテゴリ</translation>
+    </message>
+</context>
+<context>
+    <name>BufferListScreen</name>
+    <message>
+        <location filename="../BufferListScreen.qml" line="98"/>
+        <source>Lable1</source>
+        <translation>ラベル1</translation>
+    </message>
+    <message>
+        <location filename="../BufferListScreen.qml" line="111"/>
+        <source>Label2</source>
+        <translation>ラベル2</translation>
+    </message>
+    <message>
+        <location filename="../BufferListScreen.qml" line="124"/>
+        <source>Label3</source>
+        <translation>ラベル3</translation>
+    </message>
+    <message>
+        <location filename="../BufferListScreen.qml" line="137"/>
+        <source>Label4</source>
+        <translation>ラベル4</translation>
+    </message>
+    <message>
+        <location filename="../BufferListScreen.qml" line="151"/>
+        <source>Go Back</source>
+        <translation>戻ってください</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="234"/>
+        <source>Start</source>
+        <translation>開始</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="249"/>
+        <source>Stop</source>
+        <translation>停止</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="263"/>
+        <source>Store Mode</source>
+        <translation>ストアモード</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="277"/>
+        <source>Label4</source>
+        <translation>ラベル4</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="290"/>
+        <source>Label5</source>
+        <translation>ラベル5</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="304"/>
+        <source>Label6</source>
+        <translation>ラベル6</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="394"/>
+        <source>English</source>
+        <translation>英語</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="395"/>
+        <source>Chinese</source>
+        <translation>中国語</translation>
+    </message>
+</context>
+</TS>
